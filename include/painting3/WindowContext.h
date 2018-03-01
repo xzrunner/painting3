e6f68e566ff24d801726e4f239109b031ce3add6
zz <zhuguang@ejoy.com>
2018-03-01 10:57:05 +0800

    [ADDED] wnd ctx from n3

---
+++ include/painting3/WindowContext.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <SM_Matrix.h>
+
+namespace pt3
+{
+
+class WindowContext
+{
+public:
+	WindowContext();
+
+	void SetModelView(const sm::mat4& mt);
+	void SetProjection(const sm::mat4& mt);
+	void SetScreen(int width, int height);
+
+	void UpdateMVP() const;
+	void UpdateModelView() const;
+	void UpdateProjection() const;
+	void UpdateViewport() const;
+
+	const sm::mat4& GetModelViewMat() const { return m_mv_mat; }
+
+private:
+	sm::mat4 m_mv_mat;
+	sm::mat4 m_proj_mat;
+
+	int m_screen_width;
+	int m_screen_height;
+
+}; // WindowContext
+
+}
---
+++ source/WindowContext.cpp
@@ -0,0 +1,74 @@
+#include "painting3/WindowContext.h"
+
+#include <shaderlab/SubjectMVP3.h>
+#include <shaderlab/Blackboard.h>
+#include <shaderlab/RenderContext.h>
+#include <unirender/RenderContext.h>
+#include <unirender/Blackboard.h>
+
+namespace pt3
+{
+
+WindowContext::WindowContext()
+	: m_screen_width(0)
+	, m_screen_height(0)
+{
+}
+
+void WindowContext::SetModelView(const sm::mat4& mt)
+{
+	if (m_mv_mat == mt) {
+		return;
+	}
+
+	m_mv_mat = mt;
+
+	UpdateModelView();
+}
+
+void WindowContext::SetProjection(const sm::mat4& mt)
+{
+	if (m_proj_mat == mt) {
+		return;
+	}
+
+	m_proj_mat = mt;
+
+	UpdateProjection();
+}
+
+void WindowContext::SetScreen(int width, int height)
+{
+	m_screen_width = width;
+	m_screen_height = height;
+
+	UpdateViewport();
+}
+
+void WindowContext::UpdateMVP() const
+{
+	UpdateModelView();
+	UpdateProjection();
+}
+
+void WindowContext::UpdateModelView() const
+{
+	sl::Blackboard::Instance()->GetRenderContext().GetSubMVP3().NotifyModelview(m_mv_mat);
+}
+
+void WindowContext::UpdateProjection() const
+{
+	sl::Blackboard::Instance()->GetRenderContext().GetSubMVP3().NotifyProjection(m_proj_mat);
+}
+
+void WindowContext::UpdateViewport() const
+{
+	if (m_screen_width == 0 && m_screen_height == 0) {
+		return;
+	}
+
+	auto& ur_rc = ur::Blackboard::Instance()->GetRenderContext();
+	ur_rc.SetViewport(0, 0, m_screen_width, m_screen_height);
+}
+
+}
