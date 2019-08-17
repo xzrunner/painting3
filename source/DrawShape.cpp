1a1ba931d78d0d31afa159d79f23b5fcf2f0e6c6
zz <xzrunner@gmail.com>
2019-08-17 11:18:32 +0800

    [ADDED] draw PointSet3D

--- source/DrawShape.cpp
+++ source/DrawShape.cpp
@@ -3,6 +3,7 @@
 
 #include <geoshape/config.h>
 #include <geoshape/Point3D.h>
+#include <geoshape/PointSet3D.h>
 #include <geoshape/Polyline3D.h>
 #include <tessellation/Painter.h>
 
@@ -21,6 +22,14 @@ void DrawShape::Draw(tess::Painter& pt, const gs::Shape3D& shape,
     {
         auto& p3d = static_cast<const gs::Point3D&>(shape);
         pt.AddCircleFilled(trans3d(p3d.GetPos()), gs::NODE_QUERY_RADIUS, 0x88888888);
+    }
+    else if (type == rttr::type::get<gs::PointSet3D>())
+    {
+        auto& pos_set = static_cast<const gs::PointSet3D&>(shape);
+        auto& vertices = pos_set.GetVertices();
+        for (auto& v : vertices) {
+            pt.AddCircleFilled(trans3d(v), gs::NODE_QUERY_RADIUS, 0x88888888);
+        }
     }
     else if (type == rttr::type::get<gs::Polyline3D>())
     {
